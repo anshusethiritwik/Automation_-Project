a324f5dfa85f1957fdc58c5ccedaf5682e47ac01
anshusethiritwik <74006851+anshusethiritwik@users.noreply.github.com>
2023-01-17 05:34:41 +0530

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -10,7 +10,7 @@ else
         apt install  apache2 -y
 fi
 
-if [ $(systemctl status apache2 | grep -v grep | grep 'apache2 is running' | wc$
+if [ $(systemctl status apache2 | grep -v grep | grep 'apache2 is running' | wc -l) -gt 0 ];
 then
         echo "running"
 else
@@ -25,30 +25,24 @@ else
         systemctl status apache2
 fi
 
+
+sudo apt install awscli -y
+if [ $? -eq 0 ];
+then
+        echo "AWS CLI installed"
+else
+        echo "not installed"
+        exit 1
+fi
+
 timestamp=$(date '+%d%m%Y-%H%M%S')
 echo ${timestamp}
 
 cd /var/log/apache2
-tar -cf ${name}.tar *.log
-tar -cf/tmp/${name}-httpd-logs-${timestamp}.tar *.log
-if [[ -f /tmp/${name}-htttpd-logs-${timestamp}.tar ]];
+tar -cf /tmp/${name}-httpd-logs-${timestamp}.tar *.log
+
+if [[ -f /tmp/${name}-httpd-logs-${timestamp}.tar ]];
 then
-        aws s3 cp /tmp/${name}-httpd-logs-${timestamp}.tar s3 ://${s3_bucket}/${name}--https-logs-${timestamp}.tar
+        aws s3 cp /tmp/${name}-httpd-logs-${timestamp}.tar s3://${s3_bucket}/${name}-httpd-logs-${timestamp}.tar
 fi
 
-invpath="/var/www/html"
-if [[ ! -f ${invpath}/inventory.html ]];
-then
-        echo -e 'Log Type\t-\tTime Created\t-\tType\t-\tSize' > ${invpath}/inventory.html
-fi
-
-if [[ -f ${invpath}/inventory.html ]];
-then
-        size=$(du -h /tmp/${name}-httpd-logs-${timestamp}.tar | awk '{print $1}')
-        echo -e "httpd-logs\t-\t${timestamp}\t-\ttar\t-\t${size}" >> ${invpath}/inventory.html
-fi
-
-if [[ ! -f /etc/cron.d/automation ]];
-then
-        echo "* * * * * root/root/automation.sh" >> /etc/cron.d/automation
-fi
